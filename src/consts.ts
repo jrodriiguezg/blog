1f3e6994cef816758699260783a97d78edaca344
jrodriiguezg <jrodriiguezgmaker@gmail.com>
2026-05-28 20:55:53 +0200

    favicon añadido

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "jrodriiguezg.link";
+export const SITE_TITLE = "The Jrodriiguezg.link Blog";
 export const SITE_DESCRIPTION = "Bienvenido a mi blog de electrónica e informática.";
