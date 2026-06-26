814761c6d576b347a92936a09c561b5f7c658769
jrodriiguezg <jrodriiguezgmaker@gmail.com>
2026-06-26 11:25:15 +0200

    post: new

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,9 @@
 
 export const SITE_TITLE = "The Jrodriiguezg.link Blog";
 export const SITE_DESCRIPTION = "Bienvenido a mi blog de electrónica e informática.";
+
+// Configuración de Giscus para comentarios
+export const GISCUS_REPO = "jrodriiguezg/blog";
+export const GISCUS_REPO_ID = "R_kgDOSphwgw";
+export const GISCUS_CATEGORY = "General";
+export const GISCUS_CATEGORY_ID = "DIC_kwDOSphwg84C_7Nz"; // ID de categoría obtenido de giscus.app
